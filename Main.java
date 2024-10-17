de1e629303221793f95ca2867c109343077026d7
jeancarlos1808 <solanosagaljeancarlos@gmail.com>
2024-10-17 11:25:19 +0200

    v1.0

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main{
-         public static void main (String[] args {
-                    System.out.println("");
+         public static void main (String[] args) {
+		ArrayList<String> array=new ArrayListo<String>();
+		array.add(args);
+		for(String arg:array);
+                    System.out.println(arg);
+		}
           }
 }
